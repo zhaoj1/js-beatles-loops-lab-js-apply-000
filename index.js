fd55a206149e116de40752857794f3ca9a757caf
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-19 18:45:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var arr = [];
+  for(i=0,i<musicians.length,i++){
+    arr.push(${musicans}+" plays "+${instruments}+", ");
+    
+  }
+}
